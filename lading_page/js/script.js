0cce747017b62bd4753c10c36968734c116b2bef
giovanicorral <giovani.corral@gmail.com>
2023-03-30 18:23:14 -0300

    Create carousel and alert javascript

---
+++ lading_page/js/script.js
@@ -0,0 +1,10 @@
+window.onload = function (){
+
+    const enviar = document.getElementById("enviar");
+
+    enviar.addEventListener("click", teste);    
+    function teste (){
+        alert("Obrigado, entraremos em contato");
+    }
+
+}
